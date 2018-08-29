40485d1012da7ff7a51af137485d91fd37b4676b
juniorsilva17 <juniorf.silva17@gmail.com>
2018-08-29 15:26:39 -0300

    Add files via upload
    API Rest

---
+++ GestaoProdutos/src/main/java/com/produtos/apirest/GestaoProdutosApplication.java
@@ -0,0 +1,12 @@
+package com.produtos.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoProdutosApplication.class, args);
+	}
+}
